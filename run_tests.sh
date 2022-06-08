6169ca9ffe2eafba1c64ea9255ee9e2811e5128e
Geoff Dutton <g.dutton@gmail.com>
2022-06-08 11:53:04 -0500

    upgrade to dj_rest_auth and djangorestframework-simplejwt

--- run_tests.sh
+++ run_tests.sh
@@ -4,5 +4,6 @@ export EXTERNAL_POSTGRES_PORT=5499
 export EXTERNAL_REDIS_PORT=6399
 export EXTERNAL_BACKEND_PORT=8999
 docker-compose -p smart_backend_tests down
+docker-compose -p smart_backend_tests build
 docker-compose -p smart_backend_tests run --rm smart_backend ./run_tests.sh
 docker-compose -p smart_backend_tests down -v
