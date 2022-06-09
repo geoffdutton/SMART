4f7d431f5e27e6cd802f65bf6f7bd967390f3f75
Geoff Dutton <g.dutton@gmail.com>
2022-06-09 18:45:42 -0500

    only collect coverage from frontend/src/

--- frontend/jest.config.js
+++ frontend/jest.config.js
@@ -1,6 +1,9 @@
 /* eslint-env node */
 /** @type {import('@jest/types').Config.InitialOptions} */
 const config = {
+    collectCoverageFrom: [
+        "<rootDir>/src/**/*.{js,jsx}",
+    ],
     testEnvironment: "jsdom",
     setupFilesAfterEnv: ["<rootDir>/jest-setup.js"],
     moduleNameMapper: {
