8f43c03c04d16b16386b082c59e540b95c012471
Geoff Dutton <g.dutton@gmail.com>
2022-06-09 18:42:36 -0500

    add jest-fetch-mock for main smart.jsx spec

--- frontend/jest-setup.js
+++ frontend/jest-setup.js
@@ -1,1 +1,2 @@
 import "@testing-library/jest-dom";
+window.PROJECT_ID = 3443;
--- frontend/src/components/Smart/smart.spec.js
+++ frontend/src/components/Smart/smart.spec.js
@@ -2,30 +2,54 @@ import React from "react";
 import createSmartStore from "@/store";
 import { renderWithStore } from "#test-utils";
 import Smart from "./index";
+import { enableFetchMocks } from "jest-fetch-mock";
+
+enableFetchMocks();
 
 const initialState = undefined;
 
-describe.skip("<Smart />", () => {
-    /** @type {import('redux').Store} */
+describe("<Smart />", () => {
+    /** @type {import("redux").Store} */
     let store;
     let comp;
     let adminCounts;
     beforeEach(() => {
+        fetchMock.resetMocks();
         store = createSmartStore(initialState);
         adminCounts = {};
     });
 
     it("renders properly if all props provided", () => {
-        const fn = () => {};
+        fetchMock.mockAbort();
+        const fn = () => {
+        };
         comp = renderWithStore(
             <Smart
-                adminTabsAvailable = {false}
-                getAdminTabsAvailable = {fn}
-                admin_counts = {adminCounts}
-                getAdminCounts = {fn}
+                adminTabsAvailable={false}
+                getAdminTabsAvailable={fn}
+                admin_counts={adminCounts}
+                getAdminCounts={fn}
             />,
             { store }
         );
-        expect(comp).not.toBeNull();
+
+        expect(comp)
+            .not
+            .toBeNull();
+        expect(fetchMock)
+            .toHaveBeenCalledTimes(1);
+
+        expect(fetchMock)
+            .toHaveBeenCalledWith(
+                "/api/get_card_deck/3443/",
+                { credentials: "same-origin",
+                    headers: {
+                        Accept: "application/json",
+                        "Content-Type": "application/json",
+                        "X-CSRFToken": null
+                    },
+                    method: "GET"
+                }
+            );
     });
 });
